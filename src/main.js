22de81f069973ffabff510a33a5af6472e772ac4
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-08-18 02:40:03 +0430

    chore: add axios

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import { createApp as createVueApp } from 'vue'
-import App from '@/App.vue'
-import '@/assets/scss/style.scss'
-
-import router from '@/router'
+import { createApp as createVueApp } from 'vue';
+import App from '@/App.vue';
+import '@/assets/scss/style.scss';
+import  axiosPlugin  from '@/plugins/axios.js';
+import router from '@/router';
+import { createPinia } from 'pinia';
 
 const createApplication = () => {
-  const vueAppInstance = createVueApp(App)
+  const vueAppInstance = createVueApp(App);
 
-  vueAppInstance.use(router)
-  vueAppInstance.mount('#app')
-}
+  const pinia = createPinia();
+  vueAppInstance.use(pinia);
 
-createApplication()
+  vueAppInstance.use(router);
+  vueAppInstance.use(axiosPlugin);
+  vueAppInstance.mount('#app');
+};
+
+createApplication();
