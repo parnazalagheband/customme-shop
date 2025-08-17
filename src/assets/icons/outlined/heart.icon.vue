7a02c39bbe8c1300c42340e68efb7a90539921bf
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-08-18 02:42:46 +0430

    feat: impleament base-heart component

---
+++ src/assets/icons/outlined/heart.icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="red"
+    stroke="red"
+    stroke-width="2"
+    class="heart"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 21C12 21 4 13.5 4 8.5C4 6.01472 6.01472 4 8.5 4C10.04 4 11.54 4.81 12 6C12.46 4.81 13.96 4 15.5 4C17.9853 4 20 6.01472 20 8.5C20 13.5 12 21 12 21Z"
+    />
+  </svg>
+</template>
---
+++ src/components/common/button/base-heart.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="heart-container" @click="toggleLike">
+    <base-icon
+      v-if="!props.modelValue"
+      icon-name="heart"
+      style-type="outlined"
+      :stroke="'gray'"
+      :fill="'transparent'"
+      class="heart-icon"
+    />
+    <base-icon
+      v-else
+      icon-name="heart"
+      style-type="filled"
+      :stroke="'red'"
+      :fill="'red'"
+      class="heart-icon"
+    />
+  </div>
+</template>
+
+<script setup>
+  import BaseIcon from '@/components/common/icon/base-icon.vue';
+
+  const props = defineProps({
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  });
+
+  const emit = defineEmits(['update:modelValue']);
+
+  const toggleLike = () => {
+    emit('update:modelValue', !props.modelValue);
+  };
+</script>
+
+<style scoped lang="scss">
+  .heart-container {
+    cursor: pointer;
+    .heart {
+      --base-icon-size: 1.5rem;
+    }
+  }
+</style>
