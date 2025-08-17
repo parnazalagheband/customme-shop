a4fb10d4a8821b9b10e3446da22fdb87ab4aaed0
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-08-18 03:53:43 +0430

    fix: fix loading

--- src/components/common/router/global-loader.vue
+++ src/components/common/router/global-loader.vue
@@ -9,27 +9,25 @@
   const productStore = useProductStore();
 </script>
 
-<style scoped>
-.global-loader {
-  position: fixed;
-  inset: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: rgba(255,255,255,0.7);
-  z-index: 9999;
-}
-.spinner {
-  width: 40px;
-  height: 40px;
-  border: 4px solid var(--palette-gray-4);
-  border-top-color: var(--palette-primary);
-  border-radius: 50%;
-  animation: spin 1s linear infinite;
-}
-@keyframes spin {
-  100% {
-    transform: rotate(360deg);
+<style lang="scss" scoped>
+  .global-loader {
+    @include flex($align: center, $justify: center);
+    position: fixed;
+    inset: 0;
+    background: rgba(255, 255, 255, 0.7);
+    z-index: 9999;
   }
-}
+  .spinner {
+    width: 40px;
+    height: 40px;
+    border: 4px solid var(--palette-gray-4);
+    border-top-color: var(--palette-primary);
+    border-radius: $circle;
+    animation: spin 1s linear infinite;
+  }
+  @keyframes spin {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 </style>
