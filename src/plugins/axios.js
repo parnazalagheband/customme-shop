d4fc32d4a16f5d415a8b983a132aa541d01a225e
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-08-18 02:45:45 +0430

    chore: add axios plugin

---
+++ src/plugins/axios.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://demo.spreecommerce.org/api/v2/storefront',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+});
+
+export default api;
+
+export const axiosPlugin = {
+  install: (app) => {
+    app.config.globalProperties.$axios = api;
+  },
+};
