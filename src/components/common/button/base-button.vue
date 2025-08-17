3a86bc3204be5c9f7c507b596db7048bb7f81822
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-08-18 02:46:45 +0430

    fix: change base-button

--- src/components/common/button/base-button.vue
+++ src/components/common/button/base-button.vue
@@ -21,92 +21,102 @@
 </template>
 
 <script setup>
-  import { computed } from 'vue';
-  import BaseIcon from '@/components/common/icon/base-icon.vue';
+import { computed } from 'vue'
+import BaseIcon from '@/components/common/icon/base-icon.vue'
 
-  const props = defineProps({
-    variant: {
-      type: String,
-      default: 'text',
-      validator(value) {
-        return ['filled', 'outlined', 'text'].includes(value);
-      },
+const props = defineProps({
+  variant: {
+    type: String,
+    default: 'text',
+    validator(value) {
+      return ['filled', 'outlined', 'text', 'underline'].includes(value);
     },
-    leadingIcon: String,
-    trailingIcon: String,
-    leadingIconStyleType: {
-      type: String,
-    },
-    trailingIconStyleType: {
-      type: String,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-  });
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+  leadingIcon: String,
+  trailingIcon: String,
+  leadingIconStyleType: String,
+  trailingIconStyleType: String,
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+})
 
-  const paddings = computed(() => ({
-    leading: props.leadingIcon ? '16px' : '24px',
-    trailing: props.trailingIcon ? '16px' : '24px',
-  }));
+const paddings = computed(() => ({
+  leading: props.leadingIcon ? '16px' : '24px',
+  trailing: props.trailingIcon ? '16px' : '24px',
+}))
 
-  const buttonVariantClasses = computed(() => {
-    return ['button', `button_${props.variant}`];
-  });
-
-
+const buttonVariantClasses = computed(() => {
+  return [
+    'button',
+    `button_${props.variant}`,
+    { active: props.active } 
+  ]
+})
 </script>
 
 <style lang="scss" scoped>
-  .button {
-    @include typography(button-2);
-    position: relative;
-    border-radius: $pill;
-    height: 40px;
-    --base-icon-size: 1.125rem;
+.button {
+  @include typography(button-2);
+  border-radius: $radius-2x;
+  height: 40px;
+  --base-icon-size: 1.125rem;
+  color: var(--base-button-color);
+  cursor: pointer;
+  user-select: none;
+  overflow: hidden;
+  min-width: var(--base-button-min-width, 88px);
+
+  &__leading-icon,
+  &__trailing-icon {
+    flex-shrink: 0;
+    --base-icon-size: 1.5rem;
+    color: var(--palette-gray-9);
+  }
+
+  padding-left: v-bind('paddings.trailing');
+  padding-right: v-bind('paddings.leading');
+
+  &__content {
+    @include flex($align: center, $justify: center);
+    gap: space(2);
+  }
+
+  &_filled {
+    background-color: var(--base-button-color);
+    color: var(--palette-white);
+  }
+
+  &_outlined {
+    background-color: transparent;
     color: var(--base-button-color);
-    cursor: pointer;
-    user-select: none;
-    overflow: hidden;
-    min-width: var(--base-button-min-width, 88px);
+    border: 1px solid var(--base-button-color);
+  }
 
-    &__leading-icon,
-    &__trailing-icon {
-      flex-shrink: 0;
-      --base-icon-size: 1.5rem;
-      color:var(--palette-gray-9);
+  &_underline {
+    @include typography(body-3);
+    background-color: transparent;
+    color: var(--palette-gray-10);
+    border-radius: 0;
 
-    }
-
-    padding-left: v-bind('paddings.trailing');
-    padding-right: v-bind('paddings.leading');
-
-    &__content {
-      position: relative;
-      z-index: 1;
-      @include flex($align: center, $justify: center);
-      gap: space(2);
-    }
-
-    &_filled {
-      background-color: var(--palette-primary);
-      color:var(--palette-white);
-    }
-
-    &_outlined {
-      background-color: transparent;
-      color:var(--palette-primary);
-      border:1px solid var(--palette-primary);
-    }
-
-    &_text {
-      background-color: transparent;
-    }
-
-    &:disabled {
-      opacity: 0.38;
-      pointer-events: none;
+    &.active {
+      color:var(--base-button-color); 
+      border-bottom: 2px solid var(--base-button-color); 
     }
   }
+
+  &_text {
+    background-color: transparent;
+  }
+
+  &:disabled {
+    opacity: 0.38;
+    pointer-events: none;
+  }
+}
 </style>
